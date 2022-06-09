6d0a318a134fa0913886ab9f522da8ff428efa14
Smileycorp <callum.j.longbottom@gmail.com>
2022-06-09 23:43:43 +0100

    fix merge conflicts from main

--- src/main/java/net/smileycorp/ldoh/common/world/ModWorldGen.java
+++ src/main/java/net/smileycorp/ldoh/common/world/ModWorldGen.java
@@ -3,7 +3,7 @@ package net.smileycorp.ldoh.common.world;
 import java.util.Random;
 
 import net.minecraft.block.Block;
-import net.minecraft.init.Biomes;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -16,32 +16,40 @@ import net.minecraft.world.gen.feature.WorldGenerator;
 import net.minecraftforge.fml.common.IWorldGenerator;
 import net.smileycorp.ldoh.common.block.LDOHBlocks;
 import net.smileycorp.ldoh.common.tile.TileHordeSpawner;
-import biomesoplenty.api.biome.BOPBiomes;
-
-import com.legacy.wasteland.world.WastelandWorld;
+import net.smileycorp.ldoh.common.util.EnumBiomeType;
+import net.smileycorp.ldoh.common.world.WorldGenSurface.EnumVariant;
+import biomesoplenty.api.block.BOPBlocks;
+import biomesoplenty.common.block.BlockBOPDirt;
+import biomesoplenty.common.block.BlockBOPDirt.BOPDirtType;
 
 public class ModWorldGen implements IWorldGenerator {
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public void generate(Random rand, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider) {
 		//generate horde spawning blocks
 		int x = (chunkX << 4) +rand.nextInt(16);
 		int z = (chunkZ << 4) + rand.nextInt(16);
 		Biome biome = world.getBiomeProvider().getBiomes(null, x, z, 1, 1, false)[0];
-		if (rand.nextInt(biome == WastelandWorld.apocalypse_city ? 7 : biome == BOPBiomes.wasteland.get() ? 13 : biome == Biomes.DEEP_OCEAN ? 24 : 18) == 0) {
+		if (rand.nextInt(EnumBiomeType.CITY.matches(biome) ? 7 : EnumBiomeType.BADLANDS.matches(biome) ? 13
+				: EnumBiomeType.OCEAN.matches(biome) ? 24 : 18) == 0) {
 			BlockPos pos = new BlockPos(x, world.getHeight(x, z)+1, z);
 			world.setBlockState(pos, LDOHBlocks.HORDE_SPAWNER.getDefaultState(), 18);
 			((TileHordeSpawner)world.getTileEntity(pos)).setNatural();
 		}
 		BlockPos chunkpos = new BlockPos(x, 0 , z);
 		//adds our custom oregen to biomes other than the apocalyptic desert
-		if (biome != WastelandWorld.apocalypse_desert) {
+		if (!EnumBiomeType.DESERT.matches(biome)) {
 			genOre(world, chunkpos, rand, Blocks.COAL_ORE);
 			genOre(world, chunkpos, rand, Blocks.IRON_ORE);
 			genOre(world, chunkpos, rand, Blocks.GOLD_ORE);
 		}
-		if (biome == Biomes.DEEP_OCEAN && rand.nextInt(15) == 0) {
-			genSurfaceBlock(world, rand, chunkX, chunkZ);
+		if (EnumBiomeType.OCEAN.matches(biome) && rand.nextInt(15) == 0) {
+			EnumVariant variant = EnumVariant.values()[new Random().nextInt(EnumVariant.values().length)];
+			genSurfaceBlock(world, rand, chunkX, chunkZ, variant.state1, variant.state2);
+		} else if (EnumBiomeType.BADLANDS.matches(biome) && rand.nextInt(25) == 0) {
+			genSurfaceBlock(world, rand, chunkX, chunkZ, Blocks.SOUL_SAND.getDefaultState(), BOPBlocks.dirt.getDefaultState()
+					.withProperty(BlockBOPDirt.VARIANT, BOPDirtType.LOAMY).withProperty(BlockBOPDirt.COARSE, true));
 		}
 		genNest(world, biome, rand, chunkX, chunkZ);
 	}
@@ -72,15 +80,15 @@ public class ModWorldGen implements IWorldGenerator {
 		}
 	}
 
-	private void genSurfaceBlock(World world, Random rand, int chunkX, int chunkZ) {
+	private void genSurfaceBlock(World world, Random rand, int chunkX, int chunkZ, IBlockState state1, IBlockState state2) {
 		int x = (chunkX << 4) +rand.nextInt(16);
 		int z = (chunkZ << 4) + rand.nextInt(16);
-		WorldGenSurface gen = new WorldGenSurface();
+		WorldGenSurface gen = new WorldGenSurface(state1, state2);
 		gen.generate(world, rand, new BlockPos(x, world.getChunkFromChunkCoords(chunkX, chunkZ).getHeightValue(x&15, z&15)-1, z));
 	}
 
 	private void genNest(World world, Biome biome, Random rand, int chunkX, int chunkZ) {
-		if (biome != BOPBiomes.wasteland.get()) return;
+		if (!EnumBiomeType.BADLANDS.matches(biome)) return;
 		if (rand.nextInt(160) == 0) {
 			int x = (chunkX << 4) +rand.nextInt(16);
 			int z = (chunkZ << 4) + rand.nextInt(16);
