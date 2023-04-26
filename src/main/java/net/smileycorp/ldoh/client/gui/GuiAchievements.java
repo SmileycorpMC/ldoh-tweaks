a86974066f340e0e711d1263477e4754fd3bbf35
Smileycorp <callum.j.longbottom@gmail.com>
2023-04-26 15:03:41 +0100

    add screens

---
+++ src/main/java/net/smileycorp/ldoh/client/gui/GuiAchievements.java
@@ -0,0 +1,13 @@
+package net.smileycorp.ldoh.client.gui;
+
+import net.minecraft.client.gui.GuiScreen;
+
+public class GuiAchievements extends GuiScreen {
+
+	protected final GuiScreen previous;
+
+	public GuiAchievements(GuiScreen previous) {
+		this.previous = previous;
+	}
+
+}
---
+++ src/main/java/net/smileycorp/ldoh/client/gui/GuiLore.java
@@ -0,0 +1,13 @@
+package net.smileycorp.ldoh.client.gui;
+
+import net.minecraft.client.gui.GuiScreen;
+
+public class GuiLore extends GuiScreen {
+
+	protected final GuiScreen previous;
+
+	public GuiLore(GuiScreen previous) {
+		this.previous = previous;
+	}
+
+}
