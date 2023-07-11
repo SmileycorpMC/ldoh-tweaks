e4a79f36b9760cfec0ffba37405a3c8ee54c7aa6
Smileycorp <callum.j.longbottom@gmail.com>
2023-07-11 12:40:43 +0100

    improve filing cabinet renderer

--- src/main/java/net/smileycorp/ldoh/client/tesr/TESRFilingCabinet.java
+++ src/main/java/net/smileycorp/ldoh/client/tesr/TESRFilingCabinet.java
@@ -28,51 +28,78 @@ public class TESRFilingCabinet extends TileEntitySpecialRenderer<TileFilingCabin
 		Minecraft mc = Minecraft.getMinecraft();
 		EntityPlayer player = mc.player;
 		if (!te.isEmpty() && player != null) {
-			GlStateManager.pushMatrix();
-			mc.getTextureManager().bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE);
-			mc.getTextureManager().getTexture(TextureMap.LOCATION_BLOCKS_TEXTURE).setBlurMipmap(false, false);
 			EnumFacing facing = te.getWorld().getBlockState(te.getPos()).getValue(BlockFilingCabinet.FACING);
 			setLightmapDisabled(true);
 			GlStateManager.enableBlend();
 			GlStateManager.tryBlendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
 			GlStateManager.depthMask(true);
 			GlStateManager.enableAlpha();
-			switch (facing) {
-				case WEST:
-					GlStateManager.translate(x, y+0.4, z+0.5);
-					GlStateManager.rotate(-90, 0,  1, 0);
-					break;
-				case EAST:
-					GlStateManager.translate(x+1, y+0.4, z+0.5);
-					GlStateManager.rotate(90, 0,  1, 0);
-					break;
-				case NORTH:
-					GlStateManager.translate(x+0.5, y+0.4, z);
-					GlStateManager.rotate(180, 0,  1, 0);
-					break;
-				case SOUTH:
-					GlStateManager.translate(x+0.5, y+0.4, z+1);
-					break;
-			}
-			GlStateManager.scale(0.6f, 0.6f,  0.01f);
-			RenderItem render = mc.getRenderItem();
-			IBakedModel model = render.getItemModelWithOverrides(te.getContainedItem(), te.getWorld(), player);
-			model = ForgeHooksClient.handleCameraTransforms(model, ItemCameraTransforms.TransformType.GUI, false);
-			render.renderItem(te.getContainedItem(), model);
-			GlStateManager.rotate(180, 1, 0,  0);
-			GlStateManager.scale(0.05f, 0.05f,  0.05f);
-			FontRenderer font = mc.fontRenderer;
-			String count = String.valueOf(te.getCurrentCount());
-			GlStateManager.translate(0, 0, -1);
-			font.drawString(count, -font.getStringWidth(count), 2, 0xFFFFFF);
-			GlStateManager.disableRescaleNormal();
-			GlStateManager.disableLighting();
-			Minecraft.getMinecraft().getTextureManager().bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE);
-			Minecraft.getMinecraft().getTextureManager().getTexture(TextureMap.LOCATION_BLOCKS_TEXTURE).restoreLastBlurMipmap();
-			GlStateManager.color(1F, 1F, 1F, 1F);
-			GlStateManager.popMatrix();
+			drawText(mc, te, facing, x, y, z);
+			renderItem(mc, te, facing, x, y, z);
 		}
 		super.render(te, x, y, z, partialTicks, destroyStage, alpha);
+	}
+
+	private void drawText(Minecraft mc, TileFilingCabinet te, EnumFacing facing, double x, double y, double z) {
+		GlStateManager.pushMatrix();
+		switch (facing) {
+			case WEST:
+				GlStateManager.translate(x, y+0.4, z+0.5);
+				GlStateManager.rotate(-90, 0,  1, 0);
+				break;
+			case EAST:
+				GlStateManager.translate(x+1, y+0.4, z+0.5);
+				GlStateManager.rotate(90, 0,  1, 0);
+				break;
+			case NORTH:
+				GlStateManager.translate(x+0.5, y+0.4, z);
+				GlStateManager.rotate(180, 0,  1, 0);
+				break;
+			case SOUTH:
+				GlStateManager.translate(x+0.5, y+0.4, z+1);
+				break;
+		}
+		GlStateManager.rotate(180, 1, 0,  0);
+		GlStateManager.scale(0.025f, 0.025f,  0.025f);
+		GlStateManager.translate(12, 6, -0.5);
+		FontRenderer font = mc.fontRenderer;
+		String count = String.valueOf(te.getCurrentCount());
+		font.drawString(count, -font.getStringWidth(count), 2, 0xFFFFFF);
+		GlStateManager.popMatrix();
+	}
+
+	private void renderItem(Minecraft mc, TileFilingCabinet te, EnumFacing facing, double x, double y, double z) {
+		GlStateManager.pushMatrix();
+		GlStateManager.enableLighting();
+		mc.getTextureManager().bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE);
+		mc.getTextureManager().getTexture(TextureMap.LOCATION_BLOCKS_TEXTURE).setBlurMipmap(false, false);
+		switch (facing) {
+			case WEST:
+				GlStateManager.translate(x-0.01, y+0.4, z+0.5);
+				GlStateManager.rotate(-90, 0,  1, 0);
+				break;
+			case EAST:
+				GlStateManager.translate(x+1.01, y+0.4, z+0.5);
+				GlStateManager.rotate(90, 0,  1, 0);
+				break;
+			case NORTH:
+				GlStateManager.translate(x+0.5, y+0.4, z-0.01);
+				GlStateManager.rotate(180, 0,  1, 0);
+				break;
+			case SOUTH:
+				GlStateManager.translate(x+0.5, y+0.4, z+1.01);
+				break;
+		}
+		GlStateManager.scale(0.6f, 0.6f,  0.01f);
+		RenderItem render = mc.getRenderItem();
+		IBakedModel model = render.getItemModelWithOverrides(te.getContainedItem(), te.getWorld(), mc.player);
+		model = ForgeHooksClient.handleCameraTransforms(model, ItemCameraTransforms.TransformType.GUI, false);
+		render.renderItem(te.getContainedItem(), model);
+		GlStateManager.disableRescaleNormal();
+		Minecraft.getMinecraft().getTextureManager().bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE);
+		Minecraft.getMinecraft().getTextureManager().getTexture(TextureMap.LOCATION_BLOCKS_TEXTURE).restoreLastBlurMipmap();
+		GlStateManager.color(1F, 1F, 1F, 1F);
+		GlStateManager.popMatrix();
 	}
 
 }
