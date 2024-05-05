693648850381725add763fbdb9c50abc1293b057
Smileycorp <callum.j.longbottom@gmail.com>
2024-05-05 17:21:49 +0100

    lower speed modifier, increase reaver book drop chance

--- src/main/java/net/smileycorp/ldoh/common/util/DayCountSpeedModifier.java
+++ src/main/java/net/smileycorp/ldoh/common/util/DayCountSpeedModifier.java
@@ -19,7 +19,7 @@ public class DayCountSpeedModifier extends AttributeModifier {
 
     @Override
     public double getAmount() {
-        return Math.min((1 + (world.getWorldTime() / 24000)) * 0.05, 2);
+        return Math.min((1 + (world.getWorldTime() / 24000)) * 0.02, 2);
     }
 
 }
